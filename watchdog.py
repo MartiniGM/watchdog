db8006af8627d4417bc1d14028bd40a04a949a0f
Cathy Laughlin <vorfeed@gmail.com>
2015-04-18 16:23:43 -0600

    Added these to send Arduino serial output to named pipes

--- watchdog.py
+++ watchdog.py
@@ -1,23 +1,24 @@
+#!/usr/bin/python
 import serial
 from sys import exit
-import os
+import os, fcntl
 import time
 import signal
 import time    
+import select
 from contextlib import contextmanager
 from multiprocessing.dummy import Pool as ThreadPool 
 from time import sleep
 
 # ensures we can kill the script with ctrl-C for testing
 # I took this out because once I added pool.map() it just WILL NOT DIE. 
-# kill -9 it...
+# kill -9 it if using more than one Arduino...
 
-#def signal_handler(signal, frame):
-#    print('Exiting...')
-#    os._exit(0) 
+def signal_handler(signal, frame):
+    print('Exiting...')
+    os._exit(0) 
 
-#signal.signal(signal.SIGINT, signal_handler)
-
+signal.signal(signal.SIGINT, signal_handler)
 
 #wrapper function to pass into pool.map()
 def call_scan(obj): 
@@ -26,10 +27,9 @@ def call_scan(obj):
 
 class Arduino:
     
-    def __init__(self, port, outport, pin, timeout, wdtime, dogtime):
+    def __init__(self, port, pin, timeout, wdtime, dogtime):
 
- # port: input portname string, from Arduino (i.e. "/dev/arduino1", "/dev/ttyACM0", "/dev/pts/4")
- # outport: output portname string, to Max etc (i.e. "/dev/arduino1", "/dev/ttyACM0", "/dev/pts/5")
+ # port: input pipe filename, from Arduino (i.e. "/path/to/watchdog_pipe")
  # pin: GPIO output pin number to connect to Arduino reset (i.e. 1, 20)
  # timeout: seconds to serial timeout. set to 0 for non-blocking.
  # wdtime: seconds to firing watchdog (i.e. 10.0)
@@ -38,39 +38,29 @@ class Arduino:
         print "new Arduino object with port: " + port + " pin: " + str(pin) + " serial timeout: " + str(timeout) 
         print "                        watchdog sec: " + str(wdtime) + " watchdog timeout: " + str(dogtime)
         self.port = port
-        self.outport = outport
         self.pin = pin
-        self.timeout = timeout
         self.wdtime = wdtime
         self.dogtime = dogtime
         self.start = time.time()
         self.failstart = time.time()
         self.wdtimerstart = time.time()
         self.set_failstart = 0
-        self.port_opened = 0
         self.textln = ""
-        self.ser = serial.Serial()
-        self.outser = serial.Serial()
-
-    def openOutPort(self):
-        if (self.outser.isOpen() == False):
-            self.outser.baudrate = 9600
-            self.outser.port = self.outport
-            self.outser.timeout = self.timeout
-            try:
-                self.outser.open() 
-            except Exception, e:
-                print "Failed to open port " + self.outport + " : %s" % e       
+        self.not_open = 1
+        self.last_len = 0
+        try:
+            self.pipein = os.open(self.port, os.O_RDONLY | os.O_NONBLOCK)
+        except Exception, e:
+            print "Failed to open %s!" % self.port
 
     def openPort(self):
-        if (self.ser.isOpen() == False):
-            self.ser.baudrate = 9600
-            self.ser.port = self.port
-            self.ser.timeout = self.timeout
+        if (self.not_open):
             try:
-                self.ser.open() 
+                self.pipein = os.open(self.port, os.O_RDONLY | os.O_NONBLOCK)
             except Exception, e:
-                print "Failed to open port " + self.port + " : %s" % e
+                print "Failed to open %s!" % self.port
+            else:
+                self.not_open = 0;
 
     def watchdog(self):
         #print("Watchdog called on port " + self.port + " Time since last dog: " + str(time.time() - self.wdtimerstart))
@@ -88,14 +78,17 @@ class Arduino:
             try:
 #try block creates, opens, and reads serials. if any failures, retry.
                 self.openPort()
-                self.openOutPort()
-                self.textln = self.ser.read(1)
-                if (len(self.textln) != 0):
-                    print "write " + self.textln
-                    self.outser.write(self.textln)
+                #it's difficult to get non-blocking pipes in Python, but the 
+                #following works!
+                r,w,x = select.select([self.pipein],[],[],0)
+                if self.pipein in r:
+                    self.textln=os.read(self.pipein, 1)
+                    self.last_len = len(self.textln)
+                else:
+                    self.last_len = 0
             except Exception, e:
-                print "SERIAL FAILURE! %s" % e
-                self.ser.close()
+                print "PIPE FAILURE! %s" % e
+                self.not_open = 1
                 if (self.set_failstart == 0):
                     self.failstart = time.time() # records time of failure
                     self.set_failstart = 1 
@@ -106,28 +99,30 @@ class Arduino:
                     break
             #From here down, we got a read-success    
             self.set_failstart = 0  # clears the failure code in case we failed earlier 
-            if (len(self.textln) != 0):
+            if (self.last_len != 0):
                 print "Got data on port " + self.port + " in sec:" + str(time.time() - self.start) + " " + self.textln
                 self.start = time.time()
             else: 
-                # print "Warning! no data on port " + self.port + " in sec: " + str(time.time() - self.start)
+                 #print "Warning! no data on port " + self.port + " in sec: " + str(time.time() - self.start)
                 #likewise, if it's been too long since we saw data, pop the watchdog
-                if (time.time() - self.start > self.wdtime):
+                 if (time.time() - self.start > self.wdtime):
                     self.watchdog()
 
 
 print "starting in 2 seconds..."
 time.sleep(2); # give arduinos time to start
 
-# builds a list to step through the arduinos
-# use "socat -d -d PTY: PTY:" twice, then attach the other end to (or use cat)
-# "/dev/pts/5" and "/dev/pts/7" to listen in on the other end of these 
-# virtual ports. See attached "watch_serial.sh" script.
+#use serial2pipe to split the Arduino output into two named pipes, then attach
+#the following to one of the pipes, and the other to the program to be 
+#monitored!
 
-#                  input port, output port, pin, timeout, wdtime, dogtime
-arduino1 = Arduino("/dev/arduino1", "/dev/pts/4", 1, 0.0, 10.0, 40.0)
-arduino2 = Arduino("/dev/arduino2", "/dev/pts/6", 2, 0.0, 10.0, 40.0)
+#                  input filename,             pin,timeout, wdtime, dogtime
+arduino1 = Arduino("serial2pipe/watchdog_pipe", 1, 0.0, 10.0, 40.0)
+#arduino1 = Arduino("/dev/arduino1", "/dev/pts/4", 1, 0.0, 10.0, 40.0)
+arduino2 = Arduino("serial2pipe/watchdog_pipe2", 1, 0.0, 10.0, 40.0)
 #arduino3 = None
+
+# builds a list to step through the arduinos
 arduinolist = []
 arduinolist.append(arduino1)
 arduinolist.append(arduino2)
@@ -135,9 +130,12 @@ arduinolist.append(arduino2)
 
 # this creates a thread pool and assigns each working loop its own thread.
 print len(arduinolist)
-pool = ThreadPool(len(arduinolist))
-arduinolist = pool.map(call_scan, arduinolist)
+if (len(arduinolist) == 1):
+    arduino1.scan()
+else:
+    pool = ThreadPool(len(arduinolist))
+    arduinolist = pool.map(call_scan, arduinolist)
 
-# fourth version. Now it's a wrapper which writes input as soon as it arrives.
+# fifth version. Uses named pipes rather than serial comms!
 
 
