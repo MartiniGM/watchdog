12d181c65590118348e5725a57213c7a09b002a2
Cathy Laughlin <vorfeed@gmail.com>
2015-04-25 17:46:11 -0600

    Better error correction

--- serial2pipe/serial2pipe.c
+++ serial2pipe/serial2pipe.c
@@ -17,20 +17,21 @@
 #define SERIAL_DEVICE_A "/dev/cu.usbmodem1411"
 #endif
 #ifdef __linux__
-#define SERIAL_DEVICE_A "/dev/arduino1"
-//#define SERIAL_DEVICE_A "/dev/arduino2"
+//#define SERIAL_DEVICE_A "/dev/arduino1"
+#define SERIAL_DEVICE_A "/dev/arduino2"
 #endif
 
-#define PIPE_1 "watchdog_pipe1"
-#define PIPE_2 "arduino_pipe1"
-//#define PIPE_1 "watchdog_pipe2"
-//#define PIPE_2 "arduino_pipe2"
+//#define PIPE_1 "watchdog_pipe1"
+//#define PIPE_2 "arduino_pipe1"
+#define PIPE_1 "watchdog_pipe2"
+#define PIPE_2 "arduino_pipe2"
 #define BAUD B115200
 
-// global for when we need to close
+// globals for when we need to close
 int fda; FILE *fsa;
 int pipe1, pipe2;
 int num_ffs = 0;
+
 static void murder(int ignore) { 
   fclose(fsa); 
   close(fda); 
@@ -52,12 +53,13 @@ void open_ports() {
       printf("[ERROR] cannot open file %s\n", 
 	     SERIAL_DEVICE_A);
   }
+  
   // get file stream pointer for serial port to arduino
   fsa = fdopen( fda, "r");
   if (fsa < 0) 
     printf("[ERROR] cannot open stream: %s from: %d\n", 
-        SERIAL_DEVICE_A, fda);
-
+	   SERIAL_DEVICE_A, fda);
+  
   // set up options for terminal device
   struct termios toptions;
   tcgetattr(fda, &toptions);
@@ -75,8 +77,8 @@ void open_ports() {
   /* disable input/output flow control, disable restart chars */
   toptions.c_iflag &= ~(IXON | IXOFF | IXANY);
   /* disable canonical input, disable echo,
-  *  disable visually erase chars,
-  *   disable terminal-generated signals */
+   *  disable visually erase chars,
+   *   disable terminal-generated signals */
   toptions.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
   /* disable output processing */
   toptions.c_oflag &= ~OPOST;
@@ -86,75 +88,91 @@ void open_ports() {
   toptions.c_cc[VTIME] = 0;
   /* commit the options */
   tcsetattr(fda, TCSANOW, &toptions);
-
+  
 }
 
 void open_pipes() {
   printf("Started. Please start programs to listen on %s and %s. serial2pipe will now block (i.e. wait forever!) until both reader programs have been started.\n\n", PIPE_1, PIPE_2);
   fflush(stdout);
   //opens named pipes
-   pipe1 = open(PIPE_1, O_WRONLY);
-   printf("Reader successfully connected to %s\n", PIPE_1);
-   pipe2 = open(PIPE_2, O_WRONLY);
-   printf("Reader successfully connected to %s\n", PIPE_2);
-
-   if (pipe1 == 0) {
-     printf("Error! Can't open named pipe %s\n", PIPE_1);
-     exit(-1);
-   }
-
-   if (pipe2 == 0) {
-     printf("Error! Can't open named pipe %s\n", PIPE_2);
-     exit(-1);
-   }
+  pipe1 = open(PIPE_1, O_WRONLY);
+  printf("Reader successfully connected to %s\n", PIPE_1);
+  pipe2 = open(PIPE_2, O_WRONLY);
+  printf("Reader successfully connected to %s\n", PIPE_2);
+  
+  if (pipe1 == 0) {
+    printf("Error! Can't open named pipe %s\n", PIPE_1);
+    exit(-1);
+  }
+  
+  if (pipe2 == 0) {
+    printf("Error! Can't open named pipe %s\n", PIPE_2);
+    exit(-1);
+  }
 }
 
 int main(void)
 {
   char tmp;
-
+  int stat1 = 0, stat2 = 0;
   // install signal handler
   if (signal(SIGINT, murder) == SIG_ERR)
     printf("[FAILED] to install SIGINT handle\n");
-
+  
   //ignore SIGPIPE. Necessary to keep broken pipes from killing the program
+  //this redirects errors to the write() calls below, where they can be handled. 
   signal(SIGPIPE, SIG_IGN); 
-
+  
   open_ports();
-
+  
   open_pipes();
-
-   printf("Initialized! Beginning loop...\n");
-
+  
+  printf("Initialized! Beginning loop...\n");
+  
   while(1)  // our loop
     //grabs chars off the arduino serial port, writes to both pipes.
-  {
-    tmp = getc(fsa);
-
-    if (tmp == EOF) {
-      printf("SERIAL ERROR\n");
-      open_ports();
-    }
-
-    //for whatever reason, Arduino sends a flood of 0xFFs if you pull the USB cord out,
-    //not EOF. So, um, don't send more than 10 consecutive 0xFFs in your data...
-    if (tmp == 0xFF) {
-        num_ffs++;
-    } else
-    	num_ffs = 0;
-    
-    if (num_ffs >= 10) {
+    {
+      tmp = getc(fsa);
+      
+      if (tmp == EOF) {
 	printf("SERIAL ERROR\n");
 	open_ports();
-    }
-
-    if (tmp >=32 && tmp <= 126) //don't print unicode etc as characters!
-      printf("%c\n",tmp);
-    else
-      printf("%x\n",tmp);
-    write(pipe1, &tmp, 1);
-    write(pipe2, &tmp, 1);
-  } 
+      }
+      
+      //for whatever reason, Arduino sends a flood of 0xFFs if you pull the USB cord out,
+      //not EOF. So, um, don't send more than 10 consecutive 0xFFs in your data...
+      if (tmp == 0xFF) {
+        num_ffs++;
+      } else
+    	num_ffs = 0;
+      
+      if (num_ffs >= 10) {
+	printf("SERIAL ERROR\n");
+	open_ports();
+      }
+      
+      if (tmp >=32 && tmp <= 126) //don't print unicode etc as characters!
+	printf("%c\n",tmp);
+      else
+	printf("%x\n",tmp);
+      stat1 = write(pipe1, &tmp, 1);
+      stat2 = write(pipe2, &tmp, 1);
+      
+      if (stat1 < 0) {
+	//put error reporting to the watchdog here -- we want to know if the pipe breaks 
+	char error_str[2000];
+	sprintf(error_str, "Write error on %s:", PIPE_1);
+	perror(error_str);
+      }
+      
+      if (stat2 < 0) {
+	//put error reporting to the watchdog here -- we want to know if the pipe breaks 
+	char error_str[2000];
+	sprintf(error_str, "Write error on %s:", PIPE_2);
+	perror(error_str);
+      }
+      
+    } 
   murder(1); //if we end up here, close the pipes
   return 0;
 }
