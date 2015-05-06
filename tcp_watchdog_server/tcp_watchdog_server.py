a1385f8937dce66630262d6a1511c381e5416556
Cathy Laughlin <vorfeed@gmail.com>
2015-05-05 19:41:19 -0600

    TCP watchdog now writes Arduinos' status to mysql db

--- tcp_watchdog_server/tcp_watchdog_server.py
+++ tcp_watchdog_server/tcp_watchdog_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python 
 import socket, select
-from Tkinter import * 
+import MySQLdb
+#from Tkinter import * 
+
 connected = 1;
  
 def disconnect():
@@ -14,7 +16,14 @@ if __name__ == "__main__":
     CONNECTION_LIST = []    # list of socket clients
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
     PORT = 6666
-         
+
+    try:
+# Open database connection, create cursor
+        db = MySQLdb.connect("localhost","testuser","test623","testdb" )
+        cursor = db.cursor()
+    except Exception, e:
+        print "Can't connect to testdb! %s" % e
+ 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this has no effect, why ?
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -48,18 +57,45 @@ if __name__ == "__main__":
                     data = sock.recv(RECV_BUFFER)
                     # echo back the client message
                     if not sock:
+                        print "No socket, popping an error"
                         disconnect()
                     else:
                         print "got data: " + data
+                        data_list = data.split();
+                        status = data_list[0]
+                        #print "err " + status
+                        id_name = data_list[1]
+                        #print "id " + id_name
+                        strr = " "
+                        timestamp = strr.join(data_list[2:])
+                        #print "time " + timestamp
+                        location = "Dad\'s room"
+                        #insert & commit, otherwise rollback
+                        try:
+                            cursor.execute("""INSERT INTO ARDUINOS(ID_NAME, LOCATION, TIMESTAMP, STATUS)
+             VALUES (%s, %s, %s, %s)  
+             ON DUPLICATE KEY UPDATE
+             TIMESTAMP = VALUES(TIMESTAMP),
+             STATUS = VALUES(STATUS) ;
+      """, (id_name,location,timestamp,status))
+                            db.commit()
+                        except Exception, e:
+                            print "mysql error: %s" % e
+                            db.rollback()
+                        else:
+                            print "db_update okay!"
                         sock.send('OK ... ' + data)
                         connected = 1;
 
                 # client disconnected, so remove from socket list
                 except Exception, e:
                     print "Client (%s, %s) is offline" % addr
+                    print "due to %s" % e
                     sock.close()
                     disconnect()
                     CONNECTION_LIST.remove(sock)
                     continue
-         
+
+# disconnect from mysql server
+    db.close()         
     server_socket.close()
