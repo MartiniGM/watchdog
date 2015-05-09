bec8b1990acf3ee2858c9ea6e00a2901714a06a5
Cathy Laughlin <vorfeed@gmail.com>
2015-05-09 13:16:50 -0600

    mysql db dump for testing

---
+++ tcp_watchdog_server/test_database.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (armv7l)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	5.5.43-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ARDUINOS`
+--
+
+DROP TABLE IF EXISTS `ARDUINOS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ARDUINOS` (
+  `ID_NAME` char(50) NOT NULL,
+  `LOCATION` char(50) DEFAULT NULL,
+  `TIMESTAMP` char(100) DEFAULT NULL,
+  `STATUS` char(20) DEFAULT NULL,
+  PRIMARY KEY (`ID_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ARDUINOS`
+--
+
+LOCK TABLES `ARDUINOS` WRITE;
+/*!40000 ALTER TABLE `ARDUINOS` DISABLE KEYS */;
+INSERT INTO `ARDUINOS` VALUES ('192.168.1.107/test','Dad\'s room','3/04/2015 06:06:60','ERRDUINO_ACKCLEAR'),('raspberrypi/serial2pipe/watchdog_pipe1','Dad\'s room','May 09, 2015 13:14:15','ERRDUINO_NOREPLY'),('raspberrypi/serial2pipe/watchdog_pipe2','Dad\'s room','May 09, 2015 13:12:41','ERRDUINO_ACKCLEAR');
+/*!40000 ALTER TABLE `ARDUINOS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PIS`
+--
+
+DROP TABLE IF EXISTS `PIS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PIS` (
+  `ID_NAME` char(50) NOT NULL,
+  `LOCATION` char(50) DEFAULT NULL,
+  `TIMESTAMP` char(100) DEFAULT NULL,
+  `STATUS` char(20) DEFAULT NULL,
+  PRIMARY KEY (`ID_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PIS`
+--
+
+LOCK TABLES `PIS` WRITE;
+/*!40000 ALTER TABLE `PIS` DISABLE KEYS */;
+INSERT INTO `PIS` VALUES ('192.168.1.107','Dad\'s room','3/04/2015 06:06:60','ERRDUINO_ACKCLEAR');
+/*!40000 ALTER TABLE `PIS` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-09 13:14:28
