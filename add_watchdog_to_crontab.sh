7751a26a885956cb3435fc95379db3f3502e7472
matt <l3acon@gmail.com>
2016-01-09 18:49:52 +0000

    plus /dev/null

--- add_watchdog_to_crontab.sh
+++ add_watchdog_to_crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #adds the watchdog to the crontab, but only if it's not already in there
-command="/home/pi/RUNNING/scripts/watchdog.py"
+command="/home/pi/RUNNING/scripts/watchdog.py > /dev/null"
 job="@reboot $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
