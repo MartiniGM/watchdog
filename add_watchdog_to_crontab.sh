bd0636de5edfb7367b3c5bf1b81a6a6f1f863e49
vorfeed-rah <vorfeed@gmail.com>
2016-01-09 16:31:40 -0700

    Delete add_watchdog_to_crontab.sh

--- add_watchdog_to_crontab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#adds the watchdog to the crontab, but only if it's not already in there
-command="/home/pi/RUNNING/scripts/watchdog.py > /dev/null"
-job="@reboot $command"
-cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
