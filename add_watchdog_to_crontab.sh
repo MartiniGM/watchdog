428b6cd19d53487536930e470e31677ffff72c4f
Cathy Laughlin <vorfeed@gmail.com>
2016-01-19 00:50:36 +0000

    or now

--- add_watchdog_to_crontab.sh
+++ add_watchdog_to_crontab.sh
@@ -1,4 +1,4 @@
-crontab -l | sed "/^#.*/home/pi/RUNNING/scripts/watchdog.py/s/^#//" | crontab -
+crontab -l | grep -v "/home/pi/RUNNING/scripts/watchdog.py" | crontab -
 command="/home/pi/RUNNING/scripts/watchdog.py > /dev/null &"
 job="@reboot $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
